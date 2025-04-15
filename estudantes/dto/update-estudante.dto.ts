f1753df30da79f7f924a4252b52170e6b6d8e632
Yasmin Silva <133078071+yasminsilva30@users.noreply.github.com>
2025-04-15 16:19:49 -0300

    Delete estudantes directory

--- estudantes/dto/update-estudante.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEstudanteDto } from './create-estudante.dto';
-
-export class UpdateEstudanteDto extends PartialType(CreateEstudanteDto) {}
